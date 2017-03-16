56d40738b506eec5fe2c29feaeba378e4da52177
dannyradden <dradden@gmail.com>
2017-03-16 13:43:55 -0600

    added danny linked list

---
+++ linked_lists/linked_list_example.rb
@@ -0,0 +1,39 @@
+# Real World Example
+
+class WebHistory
+  attr_reader :current_page, :head
+
+  def initialize(val)
+    @head = Page.new(val, nil)
+  end
+
+  def new_page(val)
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    @current_page = current.next = Page.new(val, nil)
+  end
+
+  def return_history
+    elements = []
+    current = @head
+    while current.next != nil
+      elements << current
+      current = current.next
+    end
+    elements << current
+  end
+end
+
+class Page
+  attr_accessor :url, :next
+
+  def initialize(url, next_page)
+    @a_url = url
+    @next = next_page
+  end
+end
+
+require "pry"; binding.pry
+p ""
